e518046a60a02f548333ea0f74d7fd7d6ff9006b
James Williams <jamesrwilliams91@gmail.com>
2015-06-12 09:17:22 -0700

    update index.html with controllers, added controllers, added about section

--- router.js
+++ router.js
@@ -1,6 +1,8 @@
 App.Router.map(function() {
   this.resource('questions', {path:'/'});
-  this.resource('question', {path:'questions/:question_id'});
+  this.resource('question', {path:'questions/:question_id'}, function() {
+    this.resource('new-answer');
+  });
   this.resource('new-question');
 
 });
