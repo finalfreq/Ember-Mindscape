2995ced428d27466c396d06f2267dc3999907455
James Williams <jamesrwilliams91@gmail.com>
2015-06-12 11:10:14 -0700

    found issue with modal, added modal to questino page and pushed add answer outlet into modal

--- controllers/question.js
+++ controllers/question.js
@@ -1,7 +1,15 @@
 App.QuestionController = Ember.ObjectController.extend({
   actions: {
-      newAnswer: function() {
-        $("#myModal").modal('show');
+    newAnswer: function() {
+      $("#myModal").modal('show');
+    },
+    delete: function () {
+      if (confirm('Are you sure?')) {
+        this.get('model').destroyRecord();
+        this.transitionToRoute('questions');
+      }
+
     }
+
   }
 });
