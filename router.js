d0d3e224d7a732ab806cc39a3e38997a0fb3ff20
Guest User <student@epicodus.com>
2015-06-15 13:17:59 -0700

    removed bug where refreshing page was causing issues with individual question pages

--- router.js
+++ router.js
@@ -2,7 +2,7 @@ App.Router.map(function() {
   this.resource('questions', {path:'/'}, function() {
     this.resource('new-question');
   });
-  this.resource('question', {path:'questions/:question_id'}, function() {
+  this.resource('question', {path:':question_id'}, function() {
     this.resource('new-answer');
   });
   this.resource('about');
